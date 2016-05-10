e19987b7035206617376be2b8097d414d3b75813
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-10 17:22:18 +0000

    second one

---
+++ wp-content/themes/html5blank-stable/home.php
@@ -0,0 +1,118 @@
+<?php/*Template Name: Home */ ?>
+
+
+
+<header>
+      
+<!--      LABEL BEGINS HERE FOR THE FA BARS MENU OPTION-->
+      
+       <label class="fa fa-bars list" for="bars"></label>
+       
+       <input type="checkbox" id="bars">
+        
+<!--        LABEL BEGINS ENDS HERE FOR THE FA BARS MENU OPTION-->
+        
+<!--        NAV BAR LIST BEGINS HERE-->
+      <nav id="topbar">
+     
+       <ul id="under">
+          
+           <li>
+               <a href="about.html">about</a>
+           </li>
+           
+           <li>
+               <a href="courses.html">courses</a>
+           </li>
+           
+           <li>
+               <a href="workshops.html">workshops</a>
+           </li>
+           
+           <li>
+               <a href="cohort.html">cohort</a>
+           </li>
+           
+           <li class="active">
+               <a href="signup.html">signup</a>
+           </li>
+           
+           <li>
+               <a href="team.html">team</a>
+           </li>
+           
+           <li>
+               <a href="contact.html">contact</a>
+           </li>
+            
+       </ul>
+        
+       </nav>
+       
+<!--       NAV BAR ENDS HERE -->
+        
+        <span id="geekwise">
+        
+            <img src="http://i.imgur.com/p8H5Xv7.jpg" alt="#">
+        
+        </span>
+       
+   </header>
+   
+   <!--header ends here-->
+   
+   <!--  ****FOOTER BEGINS ALTHOUGH I DID NOT CHOOSE TO USE A FOOTER ATTRIBUTE HERE IDENTIFIED WITH A BOTTOM CLASS****-->
+  
+   <div class="bottom">
+     
+     
+<!--     BELOW IS THE MAIN TEXT BELOW THE CENTER IMAGE-->
+      
+       <h2>
+           fast. no fluff.
+       </h2>
+       
+       <h3>
+           we teach technology skills that you can use to make money<hr>   
+       </h3>
+       
+<!--       MAIN TEXT ENDS HERE THAT IS BELOW THE IMAGE-->
+       
+       <div class="lower">
+       
+<!--       ICONS BEGINS HERE FOR THE LEARN MORE, SIGNUP AND WHY GEEKWISE-->
+       
+       <span class="icons">
+           <img src="http://i.imgur.com/qgz2qoN.png" alt="Learn More"><br>learn more
+       </span>
+           
+       <span class="icons center">
+           <img src="http://i.imgur.com/pFznYsH.png" alt="Signup"><br>signup
+       </span>
+       
+       <span class="icons">
+           <img src="http://i.imgur.com/aUv0YKB.png" alt="Why Geekwise?"><br>why geekwise
+       </span>
+       
+<!--       ICONS END HERE -->
+       
+        </div>
+        
+   </div>
+   
+</main>
+
+</body>
+
+</html>
+   
+   
+
+
+
+
+
+
+
+
+
