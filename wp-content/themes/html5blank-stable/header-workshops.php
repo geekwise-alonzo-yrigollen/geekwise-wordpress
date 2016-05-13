ee73ef9966773b4ac92f7785cb5c0b3b6b9263c3
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-13 04:33:48 +0000

    workshops

--- wp-content/themes/html5blank-stable/header-workshops.php
+++ wp-content/themes/html5blank-stable/header-workshops.php
@@ -31,53 +31,47 @@
   </head>
 
 
-
-
 <body>
      
       <header>
-          <span>
-              <a href="index.html">
-                  <img class="logo" src="http://geekwiseacademy.com/img/logo-geekwise_small-black.png">
-              </a>
-          </span>
+          <span class="logo">
+            <a href="index.html">
+                <img id="imag" src="http://i.imgur.com/EXCIxXZ.png">
+            </a>
+        </span>
            <label class="fa fa-bars list" for="bars"></label>
            
 <!--          <input type="checkbox" id="bars">-->
-<nav class="nav" role="navigation">
-	<?php html5blank_nav(); ?>
-</nav>
     
-          <!--<div class="parent">-->
-          <!--    <div class="item">-->
-          <!--        <a href="about.html">About</a>-->
-          <!--    </div>-->
-          <!--    <div class="item">-->
-          <!--        <a href="courses.html">courses</a>-->
-          <!--    </div>-->
-          <!--    <div class=" item active_page">-->
-          <!--        <a href="workshops.html">workshops</a>-->
-          <!--    </div>-->
-          <!--    <div class="item">-->
-          <!--        <a href="cohort.html">cohort</a>-->
-          <!--    </div>-->
-          <!--    <div class="item">-->
-          <!--        <a href="team.html">team</a>-->
-          <!--    </div>-->
-          <!--    <div class="item">-->
-          <!--        <a href="contact.html">contact</a>-->
-          <!--    </div>-->
+          <div class="parent">
+              <div class="item">
+                  <a href="about.html">About</a>
+              </div>
+              <div class="item">
+                  <a href="courses.html">courses</a>
+              </div>
+              <div class=" item active_page">
+                  <a href="workshops.html">workshops</a>
+              </div>
+              <div class="item">
+                  <a href="cohort.html">cohort</a>
+              </div>
+              <div class="item">
+                  <a href="team.html">team</a>
+              </div>
+              <div class="item">
+                  <a href="contact.html">contact</a>
+              </div>
        
-          <!--    <div class="item sign">-->
-          <!--        <a href="signup.html">sign up</a>-->
-          <!--    </div>-->
-          <!--</div>  -->
-
+              <div class="item sign">
+                  <a href="signup.html">sign up</a>
+              </div>
+          </div>  
          
 <!--         COMMENCE TEXT SECTION-->
          
           <span>
-              <p>
+              <p id='call'>
                 <em>
                     no fluff. we teach skills that you can use to make money.
                 </em>
