8d948e68144a1237da914cc5f125efb649f9aef4
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-13 03:17:58 +0000

    tonight

--- wp-content/themes/html5blank-stable/about.php
+++ wp-content/themes/html5blank-stable/about.php
@@ -3,6 +3,16 @@
 
 
     
+    
+    <!--COMMENCE OWN IMAGE-->
+    <span id='own'>
+        
+        <img src="http://i.imgur.com/ur0IVmP.png"></img>
+        
+    </span>
+    
+    
+    
 <main id="main_section">
      
      
@@ -12,45 +22,45 @@
         
 <!--        COMMENCE FIRST ARTICLE-->
         
-         <article>
+        <!-- <article>-->
              
-             <div id="columnLeft">
+        <!--     <div id="columnLeft">-->
                  
-             <h2>WHAT</h2>
+        <!--     <h2>WHAT</h2>-->
                  
-             <p>Geekwise Academy is an accelerated training program for current and aspiring technologists. Geekwise Academy teaches trainees hard skills in the technology industry in only six weeks. Each course is fast-paced, rigorous, and narrowly focused on a hard skill that is presently in-demand in the technology industry.</p>
+        <!--     <p>Geekwise Academy is an accelerated training program for current and aspiring technologists. Geekwise Academy teaches trainees hard skills in the technology industry in only six weeks. Each course is fast-paced, rigorous, and narrowly focused on a hard skill that is presently in-demand in the technology industry.</p>-->
 
-             <h2>WHY</h2>
-             <p>We need more geeks. Lots of them. And good ones. It’s no secret that software and information technology are among the fastest growing industries on the planet. Despite that truth, there simply aren’t enough technologists to keep up with the demand. Not to mention that these industries offer promising careers and have mountains of jobs that are available today. Geekwise Academy exists to fill that demand by rapidly producing high quality technologists, and then connecting those same technologists to real world opportunities.</p>
-             </div>
+        <!--     <h2>WHY</h2>-->
+        <!--     <p>We need more geeks. Lots of them. And good ones. It’s no secret that software and information technology are among the fastest growing industries on the planet. Despite that truth, there simply aren’t enough technologists to keep up with the demand. Not to mention that these industries offer promising careers and have mountains of jobs that are available today. Geekwise Academy exists to fill that demand by rapidly producing high quality technologists, and then connecting those same technologists to real world opportunities.</p>-->
+        <!--     </div>-->
              
-        </article>
+        <!--</article>-->
      
 
 <!--       FIRST ARITCLE ***END***-->
        
 <!--        COMMENCE SECOND ARTICLE-->
-        <article>
+    <!--    <article>-->
             
-         <div id="columnRight">
+    <!--     <div id="columnRight">-->
              
-              <h2>HOW</h2>
+    <!--          <h2>HOW</h2>-->
              
-              <p>The subject matter for each course is selected by an employer who wants to recruit technologists right out of the Geekwise Academy classroom. Geekwise Academy then works with the employer to build a curriculum specifically designed to effectively teach that skill. Each course is then taught by an industry-expert lecturer. Every second of every course is designed to provide you with the skills that technology employers and clients need you to have.</p>
+    <!--          <p>The subject matter for each course is selected by an employer who wants to recruit technologists right out of the Geekwise Academy classroom. Geekwise Academy then works with the employer to build a curriculum specifically designed to effectively teach that skill. Each course is then taught by an industry-expert lecturer. Every second of every course is designed to provide you with the skills that technology employers and clients need you to have.</p>-->
 
-              <h2>DETAILS</h2>
+    <!--          <h2>DETAILS</h2>-->
              
-            <span id="p"><b>Length:</b>Each course lasts 6 weeks
+    <!--        <span id="p"><b>Length:</b>Each course lasts 6 weeks-->
 
-            <b>Time:</b>Each course meets from 6:00-9:00p, two nights per week
+    <!--        <b>Time:</b>Each course meets from 6:00-9:00p, two nights per week-->
 
-            <b>Location:</b>All Classes meet at Bitwise Mural District (2210 San Joaquin St., Fresno, CA 93721)
+    <!--        <b>Location:</b>All Classes meet at Bitwise Mural District (2210 San Joaquin St., Fresno, CA 93721)-->
 
-            <b>Cost:</b>Each class costs only $250</span>
+    <!--        <b>Cost:</b>Each class costs only $250</span>-->
     
-          </div>
+    <!--      </div>-->
     
-    </article>
+    <!--</article>-->
     
 <!--    SECOND ARTICLE ***END***-->
     
