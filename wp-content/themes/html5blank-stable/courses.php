c20515bc822e8a0c00030f605d456c36169776ba
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-13 03:47:52 +0000

    update

--- wp-content/themes/html5blank-stable/courses.php
+++ wp-content/themes/html5blank-stable/courses.php
@@ -4,11 +4,11 @@
 <!--      COMMENCE TOP SECTION -->
        
     <section>
-        <div id="geekwise">
-            <p>
-              <em>Geekwise Academy Courses are super-fast-paced and specifically designed to ensure that you’re learning what we’re teaching. Each course begins with a crash course in “How to Geekwise,” which will give you strategies for getting every ounce of benefit you can out of your Geekwise Academy experience. Geekwise Academy Courses deploy a social learning process that is intended to provide you with workplace communication training to go alongside the technology skill you signed up for. Courses include regular assessments to evaluate your progress and to make sure no one gets left behind. Finally, each course wraps up with a final project that is actual real world work</em>
-            </p>
-        </div> 
+        <!--<div id="geekwise">-->
+        <!--    <p>-->
+        <!--      <em>Geekwise Academy Courses are super-fast-paced and specifically designed to ensure that you’re learning what we’re teaching. Each course begins with a crash course in “How to Geekwise,” which will give you strategies for getting every ounce of benefit you can out of your Geekwise Academy experience. Geekwise Academy Courses deploy a social learning process that is intended to provide you with workplace communication training to go alongside the technology skill you signed up for. Courses include regular assessments to evaluate your progress and to make sure no one gets left behind. Finally, each course wraps up with a final project that is actual real world work</em>-->
+        <!--    </p>-->
+        <!--</div> -->
     </section>
      
      <main>
@@ -17,73 +17,72 @@
 <!--       COMMENCE TABLE -->
        
        
-        <section id="align_left">
+        <!--<section id="align_left">-->
         
       
-                <table id="table">
+        <!--        <table id="table">-->
                    
-                    <tr class="top_table">
-                        <th id="topic" >Topic</th>
-                        <th >none</th>
-                        <th >Day/Time</th>
-                        <th >Fee</th>
-                        <th >Instructor</th>
-                        <th >Sign&#45;up</th>
-                    </tr>
+        <!--            <tr class="top_table">-->
+        <!--                <th id="topic" >Topic</th>-->
+        <!--                <th >none</th>-->
+        <!--                <th >Day/Time</th>-->
+        <!--                <th >Fee</th>-->
+        <!--                <th >Instructor</th>-->
+        <!--                <th >Sign&#45;up</th>-->
+        <!--            </tr>-->
                     
-                    <tr>
-                        <td class="work"><u> Website for Beginners-Starts April 4,2016</u> <br>
-                        Website for beginners. Part I of II. Build Websites in HTML5,CSS3,Learn industry tools, <br> page contruction, industry specific terminology</td>
-                        <td><b>6 Weeks</b> April 4 thru May 11</td>
-                        <td> <b> Monday and Wednesday</b>6:00p-9:00p</td>
-                        <td><b>&#36;250</b>36 hrs. in-person instruction</td>
-                        <td>TBD</td>
-                        <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>
-                    </tr>
+        <!--            <tr>-->
+        <!--                <td class="work"><u> Website for Beginners-Starts April 4,2016</u> <br>-->
+        <!--                Website for beginners. Part I of II. Build Websites in HTML5,CSS3,Learn industry tools, <br> page contruction, industry specific terminology</td>-->
+        <!--                <td><b>6 Weeks</b> April 4 thru May 11</td>-->
+        <!--                <td> <b> Monday and Wednesday</b>6:00p-9:00p</td>-->
+        <!--                <td><b>&#36;250</b>36 hrs. in-person instruction</td>-->
+        <!--                <td>TBD</td>-->
+        <!--                <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>-->
+        <!--            </tr>-->
                     
-                    <tr>
-                        <td class="work"><u>Javascript for Beginners - Starts April 4, 2016</u> <br>Javascript for beginners. Learn how to think like a programmer. Build interactive webpages <br>in Javascript.</td>
-                        <td> <b>6 Weeks</b>April 4 thru May 11</td>
-                        <td><b>Monday and Wednesday</b>6:00p-9:00p</td>
-                        <td> <b>$250</b> <br>36 hrs. in-person instruction</td>
-                        <td>TBD</td>
-                        <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>
-                    </tr>
+        <!--            <tr>-->
+        <!--                <td class="work"><u>Javascript for Beginners - Starts April 4, 2016</u> <br>Javascript for beginners. Learn how to think like a programmer. Build interactive webpages <br>in Javascript.</td>-->
+        <!--                <td> <b>6 Weeks</b>April 4 thru May 11</td>-->
+        <!--                <td><b>Monday and Wednesday</b>6:00p-9:00p</td>-->
+        <!--                <td> <b>$250</b> <br>36 hrs. in-person instruction</td>-->
+        <!--                <td>TBD</td>-->
+        <!--                <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>-->
+        <!--            </tr>-->
                     
-                     <tr>
-                        <td class="work"><u>Database Design - Starts April 4, 2016</u> <br>For those with intermediate Excel experience. Design and build SQL databases. Schema <br> creation and modification, stored procedures, and data access control.</td>
-                        <td> <b>6 Weeks</b>April 4 thru May 11</td>
-                        <td><b>Monday and Wednesday</b>6:00p-9:00p</td>
-                        <td> <b>$250</b> <br>36 hrs. in-person instruction</td>
-                        <td>TBD</td>
-                        <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>
-                    </tr>
+        <!--             <tr>-->
+        <!--                <td class="work"><u>Database Design - Starts April 4, 2016</u> <br>For those with intermediate Excel experience. Design and build SQL databases. Schema <br> creation and modification, stored procedures, and data access control.</td>-->
+        <!--                <td> <b>6 Weeks</b>April 4 thru May 11</td>-->
+        <!--                <td><b>Monday and Wednesday</b>6:00p-9:00p</td>-->
+        <!--                <td> <b>$250</b> <br>36 hrs. in-person instruction</td>-->
+        <!--                <td>TBD</td>-->
+        <!--                <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>-->
+        <!--            </tr>-->
                     
-                     <tr>
-                        <td class="work"><u>Mobile-First Websites - Starts April 5, 2016</u> <br>Building on Websites for Beginners, Part II of II. Build responsive <br> websites. Learn responsive web concepts, CSS frameworks, advanced layout techniques with Flexbox and introduction <br> to Photoshop.</td>
-                        <td> <b>6 Weeks</b>April 5 thru May 12</td>
-                        <td><b>Tuesday and Thursday</b>6:00p-9:00p</td>
-                        <td> <b>$250</b> <br>36 hrs. in-person instruction</td>
-                        <td>TBD</td>
-                        <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>
-                    </tr>
+        <!--             <tr>-->
+        <!--                <td class="work"><u>Mobile-First Websites - Starts April 5, 2016</u> <br>Building on Websites for Beginners, Part II of II. Build responsive <br> websites. Learn responsive web concepts, CSS frameworks, advanced layout techniques with Flexbox and introduction <br> to Photoshop.</td>-->
+        <!--                <td> <b>6 Weeks</b>April 5 thru May 12</td>-->
+        <!--                <td><b>Tuesday and Thursday</b>6:00p-9:00p</td>-->
+        <!--                <td> <b>$250</b> <br>36 hrs. in-person instruction</td>-->
+        <!--                <td>TBD</td>-->
+        <!--                <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>-->
+        <!--            </tr>-->
                     
-                     <tr>
-                        <td class="work"><u>Intro to AngularJS - Starts April 5, 2016</u> <br>For intermediate Javascript programmers. Build applications using the AngularJS <br>framework.</td>
-                        <td> <b>6 Weeks</b>April 5 thru May 12</td>
-                        <td><b>Tuesday and Thursday</b>6:00p-9:00p</td>
-                        <td> <b>$250</b>36 hrs. in-person instruction</td>
-                        <td>TBD</td>
-                        <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>
-                    </tr>
+        <!--             <tr>-->
+        <!--                <td class="work"><u>Intro to AngularJS - Starts April 5, 2016</u> <br>For intermediate Javascript programmers. Build applications using the AngularJS <br>framework.</td>-->
+        <!--                <td> <b>6 Weeks</b>April 5 thru May 12</td>-->
+        <!--                <td><b>Tuesday and Thursday</b>6:00p-9:00p</td>-->
+        <!--                <td> <b>$250</b>36 hrs. in-person instruction</td>-->
+        <!--                <td>TBD</td>-->
+        <!--                <td class="sig"><a href="signup.html" class="rtsign">enroll</a></td>-->
+        <!--            </tr>-->
                     
-                </table>
+        <!--        </table>-->
                 
-            </section>
+        <!--    </section>-->
             
 <!--            TABLE ***END***-->
     
     </main>
     
 <!--    MAIN END-->
-</body>
