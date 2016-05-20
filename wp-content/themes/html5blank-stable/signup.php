88f05c9c55b3c49a45ae12888951facbd55b8aae
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-20 22:11:29 +0000

    sign up page done

--- wp-content/themes/html5blank-stable/signup.php
+++ wp-content/themes/html5blank-stable/signup.php
@@ -1,65 +1,54 @@
 <?php/*Template Name: Signup */ ?>
 <?php get_header('signup'); ?>
 
-    <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-    <?php the_content(); ?>
-    <?php endwhile; ?>
-    <?php endif; ?>
 
-THE BASIC STRUCTURE OF A TABLE
 
-   <!--<table id="table">-->
-   <!--    <tbody>-->
-   <!--       <tr id="signup">-->
-   <!--            <td>-->
-   <!--                <a href="signup.html">-->
-   <!--                    <img src="http://i.imgur.com/k4vRUqX.png">-->
-   <!--                </a>-->
-   <!--             </td>-->
-   <!--       </tr>-->
-   <!--        <tr class="table_row">-->
-   <!--            <td class="table_data">-->
-   <!--                <p>Database Design - Starts April 4, 2016</p>-->
-   <!--                 <p>Monday &amp; Wednesday</p>-->
-   <!--            </td>-->
-   <!--        </tr>-->
-   <!--        <tr class="table_row">-->
-   <!--            <td class="table_data">-->
-   <!--                <p>Into to AngularJS - Starts April 5, 2016</p>-->
-   <!--                 <p>Tuesday &amp; Thursday</p>-->
-   <!--            </td>-->
-   <!--        </tr>-->
-   <!--        <tr class="table_row">-->
-   <!--            <td class="table_data">-->
-   <!--                <p>Javascript for Beginners - Stars April 5, 2016</p>-->
-   <!--                <p>Monday &amp; Thursday-->
-   <!--            </td>-->
-   <!--        </tr>-->
-   <!--        <tr class="table_row">-->
-   <!--            <td class="table_data">-->
-   <!--                <p>Mobile-First Websites - Starts April 5, 2016</p>-->
-   <!--                 <p>Tuesday &amp; Wednesday</p>-->
-   <!--            </td>-->
-   <!--        </tr>-->
-   <!--        <tr class="table_row">-->
-   <!--            <td class="table_data">-->
-   <!--                <p>Webistes for Beginners - Starts April 4, 2016</p>-->
-   <!--                <p>Monday &amp; Wednesday</p>-->
-   <!--            </td>-->
-   <!--        </tr>-->
-   <!--    </tbody>-->
-   <!--</table>-->
+
+<!--THE BASIC STRUCTURE OF A TABLE-->
+
+                   <span id="signup">
+                      <a href="signup.html">
+                       <img src="http://i.imgur.com/k4vRUqX.png">
+                      </a>
+                   </span>
+   <table id="table">
+       <tbody>
+          <!--<tr id="signup">-->
+          <!--     <td>-->
+                   
+          <!--      </td>-->
+          <!--</tr>-->
+           <tr class="table_row">
+               <td class="table_data">
+                   <p>Database Design - Starts April 4, 2016</p>
+                    <p>Monday &amp; Wednesday</p>
+               </td>
+           </tr>
+           <tr class="table_row">
+               <td class="table_data">
+                   <p>Into to AngularJS - Starts April 5, 2016</p>
+                    <p>Tuesday &amp; Thursday</p>
+               </td>
+           </tr>
+           <tr class="table_row">
+               <td class="table_data">
+                   <p>Javascript for Beginners - Starts April 5, 2016</p>
+                   <p>Monday &amp; Thursday
+               </td>
+           </tr>
+           <tr class="table_row">
+               <td class="table_data">
+                   <p>Mobile-First Websites - Starts April 5, 2016</p>
+                    <p>Tuesday &amp; Wednesday</p>
+               </td>
+           </tr>
+           <tr class="table_row">
+               <td class="table_data">
+                   <p>Webistes for Beginners - Starts April 4, 2016</p>
+                   <p>Monday &amp; Wednesday</p>
+               </td>
+           </tr>
+       </tbody>
+   </table>
    
-TABLE STRUCTURE ***END***
-
-
-<!--MAIN SECTION IS HERE IF I NEED-->
-
-<!--
-         <main>
-  
-         </main>
--->
- 
-<!-- MAIN SECTION ***END***-->
+<!--TABLE STRUCTURE ***END***-->
