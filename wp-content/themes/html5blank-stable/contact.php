57a9b8f6ca0bf0fc3398b03c9e23699738af0c56
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-20 18:50:51 +0000

    contact page

--- wp-content/themes/html5blank-stable/contact.php
+++ wp-content/themes/html5blank-stable/contact.php
@@ -10,15 +10,13 @@
        <main>
             <img id='background' src='http://i.imgur.com/bXtIcQ2.jpg'>
            
-        <div class="backdround_image">   
+        <div class="background_image">  
+         <img src="http://i.imgur.com/F9kEM24.png">
             <span id='bitwise'>
-                <img src="http://i.imgur.com/F9kEM24.png">
-            </span>
-            <span id="text_menu">
-               <p>2210 San Joaquin Street</p>
-               <p>Fresno, California 93721</p>
+               <p class='address'>700 Van Ness Ave</p>
+               <p class='address'>Fresno, California 93721</p>
                <p>559-472-8786</p>
-               <p><a href=""> tsolis@bitwiseindustries.com</a></p>
+               <p>tsolis@bitwiseindustries.com</p>
             </span>
         </div>
            
