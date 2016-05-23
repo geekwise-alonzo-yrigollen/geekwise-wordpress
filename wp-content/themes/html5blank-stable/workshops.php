7f3848eb6aefc5613352e4e751e8528c17b28963
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-23 23:26:14 +0000

    workshops style

--- wp-content/themes/html5blank-stable/workshops.php
+++ wp-content/themes/html5blank-stable/workshops.php
@@ -13,51 +13,51 @@
             <?php the_content(); ?>
             <?php endwhile; ?>
             
-  <h5 class="fluff"></h5>
+  <h5 class="fluff">no flush we teach skills that make money</h5>
       
   
 
-<!--      <div class="text"> -->
-<!--          <h3>PROFESSIONAL GEEK SERIES</h3><hr><br>-->
-<!--          <p id="pro">Individuals are responsible for providing their own computers. All courses are live, in-person instruction conducted at Geekwise Academy, which is located at Bitwise South Stadium, 700 Van Ness Ave, Fresno, California 93721. Geekwise Academy is a division of Bitwise Industries.</p>-->
+      <div class="text"> 
+          <h3>PROFESSIONAL GEEK SERIES</h3><hr><br>
+          <p id="pro">Individuals are responsible for providing their own computers. All courses are live, in-person instruction conducted at Geekwise Academy, which is located at Bitwise South Stadium, 700 Van Ness Ave, Fresno, California 93721. Geekwise Academy is a division of Bitwise Industries.</p>
 
-<!--          <p>Workshops are free to current Geekwise Academy students &amp; $10 per session for non-students. Freelancing for Geeks has two sessions ($20 total) and Job Stuff has three sessions ($30 total). Workshops sessions cannot be sold separately; each session will build on the one before.</p><br>-->
+          <p>Workshops are free to current Geekwise Academy students &amp; $10 per session for non-students. Freelancing for Geeks has two sessions ($20 total) and Job Stuff has three sessions ($30 total). Workshops sessions cannot be sold separately; each session will build on the one before.</p><br>
 
-<!--          <h3>JOBSTUFF FOR GEEKS</h3><hr><br>-->
-<!--    <h5>Ask for Advice, Get Money Twice</h5>-->
+          <h3>JOBSTUFF FOR GEEKS</h3><hr><br>
+    <h5>Ask for Advice, Get Money Twice</h5>
 
-<!--    <p>1st Saturday of the Month 9:30am to 12:30pm</p>-->
+    <p>1st Saturday of the Month 9:30am to 12:30pm</p>
 
-<!--    <p>Draft the résumé that gets you the interview by learning how to use resources and tips to build a stellar résumé. Get step-by-step instructions and feedback from our résumé experts.</p>-->
+    <p>Draft the résumé that gets you the interview by learning how to use resources and tips to build a stellar résumé. Get step-by-step instructions and feedback from our résumé experts.</p>
 
-<!--    <h5>You Better Check Yo’self Before You Wreck Yo’self</h5>-->
+    <h5>You Better Check Yo’self Before You Wreck Yo’self</h5>
 
-<!--    <p>2nd Saturday of the Month 9:30am to 12:30pm</p>-->
+    <p>2nd Saturday of the Month 9:30am to 12:30pm</p>
 
-<!--    <p>Develop your personal profile and learn how to sell your skills. Create dynamic and interesting profiles on LinkedIn and GitHub and fine tune your résumé.</p>-->
+    <p>Develop your personal profile and learn how to sell your skills. Create dynamic and interesting profiles on LinkedIn and GitHub and fine tune your résumé.</p>
 
-<!--    <h5>So Fresh So Clean</h5>-->
+    <h5>So Fresh So Clean</h5>
 
-<!--    <p>3rd Saturday of the Month 9:30am to 12:30pm</p>-->
+    <p>3rd Saturday of the Month 9:30am to 12:30pm</p>
 
-<!--    <p>Our experts will take you through exercises in interview prep that will help you showcase your skills, so tuck in your shirt and line up that fade. We’ll teach you to kill it in the interview</p><br>-->
+    <p>Our experts will take you through exercises in interview prep that will help you showcase your skills, so tuck in your shirt and line up that fade. We’ll teach you to kill it in the interview</p><br>
 
-<!--    <h3>FREELANCING FOR GEEKS</h3><hr><br>-->
-<!--    <h5>Mo’ Money, Mo’ Problems</h5>-->
+    <h3>FREELANCING FOR GEEKS</h3><hr><br>
+    <h5>Mo’ Money, Mo’ Problems</h5>
 
-<!--    <p>1st Saturday of the Month 1pm to 4pm</p>-->
+    <p>1st Saturday of the Month 1pm to 4pm</p>
 
-<!--    <p>Learn how to do your taxes and organize that money. An expert will walk you through the basics of writing contracts, invoicing, record keeping, how to value your time and how much to charge, how to collect payment, and how to fill out W9s, W2s and 1099s.</p>-->
+    <p>Learn how to do your taxes and organize that money. An expert will walk you through the basics of writing contracts, invoicing, record keeping, how to value your time and how much to charge, how to collect payment, and how to fill out W9s, W2s and 1099s.</p>
 
-<!--    <h5>Ain’t Nobody Gonna Hold Me Down, Uh-oh!</h5>-->
+    <h5>Ain’t Nobody Gonna Hold Me Down, Uh-oh!</h5>
 
-<!--    <p>2nd Saturday of the Month 1pm to 4pm</p>-->
+    <p>2nd Saturday of the Month 1pm to 4pm</p>
 
-<!--    <p>Learn how to communicate with clients, deal with unhappy clients, manage change orders, and present work to clients. Our experts will offer practical advice and strategies to help you work with a variety of client personalities.</p>-->
-<!--    </div>-->
+    <p>Learn how to communicate with clients, deal with unhappy clients, manage change orders, and present work to clients. Our experts will offer practical advice and strategies to help you work with a variety of client personalities.</p>
+    </div>
 
-<!--    </main>-->
-<!--  footer  -->
-<!--    <footer>-->
-<!--        <p>SEATING IS LIMITED, SIGN UP TODAY</p>-->
-<!--    </footer>-->
+    </main>
+    
+    <footer>
+        <p>SEATING IS LIMITED, SIGN UP TODAY</p>
+    </footer>
