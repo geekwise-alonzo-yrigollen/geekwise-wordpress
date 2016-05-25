7648ce9b8da9e1152550a3b8e67b984072fcb22a
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-05-25 16:10:42 +0000

    done

--- wp-content/themes/html5blank-stable/home.php
+++ wp-content/themes/html5blank-stable/home.php
@@ -11,39 +11,39 @@
     
     
     
-<!--      <h2>-->
-<!--           fast. no fluff.-->
-<!--       </h2>-->
+      <h2>
+           fast. no fluff.
+       </h2>
        
-<!--       <h3>-->
-<!--           we teach technology skills that you can use to make money<hr>   -->
-<!--       </h3>-->
+       <h3>
+           we teach technology skills that you can use to make money<hr>   
+       </h3>
        
-<!--       MAIN TEXT ENDS HERE THAT IS BELOW THE IMAGE-->
+       <!--MAIN TEXT ENDS HERE THAT IS BELOW THE IMAGE-->
        
-<!--       <div class="lower">-->
+       <div class="lower">
        
-<!--       ICONS BEGINS HERE FOR THE LEARN MORE, SIGNUP AND WHY GEEKWISE-->
+       <!--ICONS BEGINS HERE FOR THE LEARN MORE, SIGNUP AND WHY GEEKWISE-->
        
-<!--       <span class="icons">-->
-<!--           <img src="http://i.imgur.com/qgz2qoN.png" alt="Learn More"><br>learn more-->
-<!--       </span>-->
+       <span class="icons">
+           <img src="http://i.imgur.com/qgz2qoN.png" alt="Learn More"><br>learn more
+       </span>
            
-<!--       <span class="icons center">-->
-<!--           <img src="http://i.imgur.com/pFznYsH.png" alt="Signup"><br>signup-->
-<!--       </span>-->
+       <span class="icons center">
+           <img src="http://i.imgur.com/pFznYsH.png" alt="Signup"><br>signup
+       </span>
        
-<!--       <span class="icons">-->
-<!--           <img src="http://i.imgur.com/aUv0YKB.png" alt="Why Geekwise?"><br>why geekwise-->
-<!--       </span>-->
+       <span class="icons">
+           <img src="http://i.imgur.com/aUv0YKB.png" alt="Why Geekwise?"><br>why geekwise
+       </span>
        
-<!--       ICONS END HERE -->
+       <!--ICONS END HERE -->
        
-<!--        </div>-->
+        </div>
         
-<!--   </div>-->
+   </div>
    
-<!--</main>-->
+</main>
     
     
     <!--below is div end for loop-->
